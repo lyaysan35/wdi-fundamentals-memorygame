85ea70edfab34b2dd48c0242f569628645eafc9b
Lyaysan Glick <lyaysanglick@MacBook-Pro.domain>
2019-08-25 20:50:15 -0600

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,29 @@
-var cards = ["queen", "queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit:"hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-hearts.png"
+},
+];
+
+
+
+
 var cardsInPlay = [];
 
  function checkForMatch(){
@@ -11,8 +36,10 @@ var cardsInPlay = [];
  }
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped "+ cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
